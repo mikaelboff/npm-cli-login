5571437ad700afaefcdcc5211cd7a5521c32fccd
soluty <lvming@dabxjj.com>
2018-10-10 10:54:59 +0800

    添加--strict指示

--- bin/npm-cli-login.js
+++ bin/npm-cli-login.js
@@ -1,33 +1,36 @@
 #!/usr/bin/env node
 
-var getArg = function (marker, isBoolean) {
-    var pos = process.argv.indexOf(marker);
-    return (pos === -1) ? -1 : (isBoolean ? pos : pos + 1);
-};
+let getArg = function(marker, isBoolean) {
+  let pos = process.argv.indexOf(marker)
+  return pos === -1 ? -1 : isBoolean ? pos : pos + 1
+}
 
-var login = function () {
-    var found = getArg('-u', false);
-    var user = (found === -1) ? process.env.NPM_USER : process.argv[found];
+let login = function() {
+  let found = getArg('-u', false)
+  let user = found === -1 ? process.env.NPM_USER : process.argv[found]
 
-    found = getArg('-p', false);
-    var pass = (found === -1) ? process.env.NPM_PASS : process.argv[found];
+  found = getArg('-p', false)
+  let pass = found === -1 ? process.env.NPM_PASS : process.argv[found]
 
-    found = getArg('-e', false);
-    var email = (found === -1) ? process.env.NPM_EMAIL : process.argv[found];
+  found = getArg('-e', false)
+  let email = found === -1 ? process.env.NPM_EMAIL : process.argv[found]
 
-    found = getArg('-r', false);
-    var registry = (found === -1) ? process.env.NPM_REGISTRY : process.argv[found];
+  found = getArg('-r', false)
+  let registry = found === -1 ? process.env.NPM_REGISTRY : process.argv[found]
 
-    found = getArg('-s', false);
-    var scope = (found === -1) ? process.env.NPM_SCOPE : process.argv[found];
+  found = getArg('-s', false)
+  let scope = found === -1 ? process.env.NPM_SCOPE : process.argv[found]
 
-    found = getArg('--config-path', false);
-    var configPath = (found === -1) ? process.env.NPM_RC_PATH : process.argv[found];
+  found = getArg('--config-path', false)
+  let configPath = found === -1 ? process.env.NPM_RC_PATH : process.argv[found]
 
-    found = getArg('--quotes', true);
-    var quotes = (found === -1) ? false : true;
+  found = getArg('--quotes', true)
+  let quotes = found === -1 ? false : true
 
-    require('../')(user, pass, email, registry, scope, quotes, configPath);
-};
+  found = getArg('--strict', true)
+  let isStrict = found === -1 ? true : false
+  console.log('is strict =', isStrict)
+  require('../')(user, pass, email, registry, scope, quotes, configPath, isStrict)
+}
 
-login();
+login()
